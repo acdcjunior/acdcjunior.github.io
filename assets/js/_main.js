067c58066416225bb8d2688f6c1d46e1ef8366f0
acdcjunior <acdcjunior@gmail.com>
2016-06-04 18:54:40 -0300

    chore(template): change main files of template

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -1,23 +1,54 @@
-/*! Plugin options and other jQuery stuff */
+/* ==========================================================================
+   jQuery plugin settings and other scripts
+   ========================================================================== */
 
-// FitVids options
-$(function() {
-	$("article").fitVids();
-});
+$(document).ready(function(){
 
-// Table of Contents toggle
-$(function() {
-  $(".toc h3").click(function () {
-    $("#drawer").toggleClass("hidden");
+  // FitVids init
+  $("#main").fitVids();
+
+  // init sticky sidebar
+  $(".sticky").Stickyfill();
+
+  var stickySideBar = function(){
+    var windowWidth = $(window).width();
+    if (windowWidth > 1024) {
+      // fix
+      Stickyfill.rebuild();
+      Stickyfill.init();
+    } else {
+      // unfix
+      Stickyfill.stop();
+    }
+  };
+
+  stickySideBar();
+
+  $(window).resize(function(){
+    stickySideBar();
   });
-});
 
-// Add lightbox class to all image links
-$("a[href$='.jpg'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+  // Follow menu drop down
 
-// Magnific-Popup options
-$(document).ready(function() {
-  $('.image-popup').magnificPopup({
+  $(".author__urls-wrapper button").on("click", function() {
+    $(".author__urls").fadeToggle("fast", function() {});
+    $(".author__urls-wrapper button").toggleClass("open");
+  });
+
+  // init smooth scroll
+  $("a").smoothScroll({offset: -20});
+
+  // add lightbox class to all image links
+  $("a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']").addClass("image-popup");
+
+  // Magnific-Popup options
+  $(".image-popup").magnificPopup({
+    disableOn: function() {
+      if( $(window).width() < 500 ) {
+        return false;
+      }
+      return true;
+    },
     type: 'image',
     tLoading: 'Loading image #%curr%...',
     gallery: {
@@ -28,9 +59,18 @@ $(document).ready(function() {
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.',
     },
-    removalDelay: 300, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open. 
+    removalDelay: 500, // Delay in milliseconds before popup is removed
+    // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
-    mainClass: 'mfp-fade'
+    mainClass: 'mfp-zoom-in',
+    callbacks: {
+      beforeOpen: function() {
+        // just a hack that adds mfp-anim class to markup
+        this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+      }
+    },
+    closeOnContentClick: true,
+    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
+
 });
